2fbaa441b0aaf908275d2f937d7e60281cda0aa6
Teja-c <dhokitejaswini@gmail.com>
2023-04-19 11:58:29 +0000

    first commit

---
+++ src/components/Message/index.js
@@ -0,0 +1,11 @@
+// Write your code her
+import './index.css'
+
+const Message = props => {
+  const {isLoggedIn} = props
+  const message = isLoggedIn ? 'Welcome User' : 'Please Login'
+
+  return <h1 className="message">{message}</h1>
+}
+
+export default Message
